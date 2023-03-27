f4d306f39e62ade737478987e070c4f800195bb8
Senka1 <40804722+Senka1@users.noreply.github.com>
2023-03-27 22:23:02 +0300

    Add files via upload

---
+++ 4.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PW42
+{
+    class Package
+    {
+        public string PackageDescription { get; set; }
+        public double Weight { get; set; }
+
+        public Package(string PackageDescription, double weight)
+        {
+            PackageDescription = PackageDescription;
+            Weight = weight;
+        }
+    }
+
+    class DeliveryService
+    {
+        public double WeightLimit { get; private set; }
+        public double TotalWeight { get; private set; }
+
+        public DeliveryService(double weightLimit)
+        {
+            WeightLimit = weightLimit;
+            TotalWeight = 0;
+        }
+
+        public void SendPackage(Package package)
+        {
+            if (TotalWeight + package.Weight > WeightLimit)
+            {
+                Console.WriteLine($"Посылка весит: ({package.Weight}) кг. Превышен лимит веса: ({WeightLimit} кг). Отправка прервана.");
+            }
+            else
+            {
+                TotalWeight += package.Weight;
+                Console.WriteLine($"Посылка отправлена: {package.PackageDescription} ({package.Weight} кг)");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeliveryService deliveryService = new DeliveryService(10);
+
+            Package package1 = new Package("Посылка1", 1);
+            deliveryService.SendPackage(package1);
+
+            Package package2 = new Package("Посылка2", 2);
+            deliveryService.SendPackage(package2);
+
+            Package package3 = new Package("Посылка3", 30);
+            deliveryService.SendPackage(package3);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4.3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Orc
+{
+    private static int orcCount;
+    private const int MaxGoldValue = 2;
+
+    public Orc()
+    {
+        orcCount++;
+    }
+
+    public int ActualGoldValue
+    {
+        get
+        {
+            if (orcCount <= 5)
+            {
+                return orcCount * MaxGoldValue;
+            }
+            else
+            {
+                return (MaxGoldValue * 5) - (orcCount - 5) * 2;
+            }
+        }
+    }
+
+    public void Output()
+    {
+        Console.WriteLine($"У {orcCount} орк(а/ов) сейчас максимальное значение золота");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Orc[] orcs = new Orc[7];
+
+        for (int i = 0; i < orcs.Length; i++)
+        {
+            orcs[i] = new Orc();
+            orcs[i].Output();
+
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(orcs[j].ActualGoldValue);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
